72a34d17070a1ba606df1619282e163767ba1b2e
Terrev <21133460+Terrev@users.noreply.github.com>
2025-04-15 14:25:28 -0400

    playing with numbers

--- dScripts/newcontent/server/SpawnMaelstromNinjaOnDeath.cpp
+++ dScripts/newcontent/server/SpawnMaelstromNinjaOnDeath.cpp
@@ -7,12 +7,12 @@
 
 LOT RollLOT() {
 	LOT chosenLOT{ LOT_NULL };
-	auto chance = GeneralUtils::GenerateRandomNumber<uint32_t>(1, 100);
+	auto chance = GeneralUtils::GenerateRandomNumber<uint32_t>(1, 200);
 	if (chance <= 35) {
 		chosenLOT = 20154;
 	} else if (chance <= 45) {
 		chosenLOT = 20155;
-	} else if (chance == 50) {
+	} else if (chance == 100) {
 		chosenLOT = 20051;
 	}
 	
