fe4794c02c9762794f8396b0ef0981529f605467
David Markowitz <EmosewaMC@gmail.com>
2025-07-15 13:31:38 -0700

    mech dont do stuff if not built

--- dScripts/ai/YRK/MechEuBroombot.cpp
+++ dScripts/ai/YRK/MechEuBroombot.cpp
@@ -1,36 +1,33 @@
 #include "MechEuBroombot.h"
 
 #include "MovementAIComponent.h"
+#include "QuickBuildComponent.h"
 #include "SkillComponent.h"
 #include "dZoneManager.h"
 #include "SkunkEvent.h"
 
 void MechEuBroombot::OnUpdate(Entity* self) {
+	auto const [skillComponent, movementAiComponent, quickbuildComponent] = self->GetComponentsMut<SkillComponent, MovementAIComponent, QuickBuildComponent>();
+
+	if (!skillComponent || !movementAiComponent || !quickbuildComponent || quickbuildComponent->GetState() != eQuickBuildState::COMPLETED) return;
+
 	// This is omega bodge but i cant really do anything better at this time
 	for (auto* const entity : Game::entityManager->GetEntitiesByProximity(self->GetPosition(), 6.0f)) {
 		if (!entity || entity->GetLOT() != SkunkEvent::INVASION_STINK_CLOUD_LOT || entity->GetVar<bool>(u"cleaned")) continue;
-		auto const [skillComponent, movementAiComponent] = self->GetComponentsMut<SkillComponent, MovementAIComponent>();
-		if (skillComponent) {
-			skillComponent->CastSkill(115, entity->GetObjectID());
-			entity->SetVar<bool>(u"cleaned", true);
-		}
-		if (movementAiComponent) {
-			movementAiComponent->Pause(5.0f);
-		}
+		skillComponent->CastSkill(115, entity->GetObjectID());
+		entity->SetVar<bool>(u"cleaned", true);
+		movementAiComponent->Pause(5.0f);
 	}
 }
 
 void MechEuBroombot::OnUse(Entity* self, Entity* builder) {
 	if (!builder) return;
+	auto const [skillComponent, movementAiComponent, quickbuildComponent] = self->GetComponentsMut<SkillComponent, MovementAIComponent, QuickBuildComponent>();
 
-	const auto [movementAiComponent, skillComponent] = self->GetComponentsMut<MovementAIComponent, SkillComponent>();
-	if (movementAiComponent) {
-		movementAiComponent->Pause(5.0f);
-	}
+	if (!skillComponent || !movementAiComponent || !quickbuildComponent || quickbuildComponent->GetState() != eQuickBuildState::COMPLETED) return;
 
-	if (skillComponent) {
-		skillComponent->CastSkill(115, builder->GetObjectID());
-	}
+	movementAiComponent->Pause(5.0f);
+	skillComponent->CastSkill(115, builder->GetObjectID());
 }
 
 void MechEuBroombot::OnQuickBuildComplete(Entity* self, Entity* builder) {
