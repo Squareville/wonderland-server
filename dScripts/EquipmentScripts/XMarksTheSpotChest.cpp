3be1d63576e744fcd9f6e147b565a5da68e5cd4b
Aaron Kimbrell <aronwk.aaron@gmail.com>
2025-10-18 22:31:45 -0500

    it lives (numbers adjusted via jamie)

--- dScripts/EquipmentScripts/XMarksTheSpotChest.cpp
+++ dScripts/EquipmentScripts/XMarksTheSpotChest.cpp
@@ -6,46 +6,45 @@
 #include "Loot.h"
 #include "TeamManager.h"
 
-    void
-    XMarksTheSpotChest::OnStartup(Entity* self) {
-    self->AddTimer("SpawnGoodies", 1);
+// modified from a scrapped rank 1 buccaneer script and the numbers and drops have been adjusted
+
+void
+XMarksTheSpotChest::OnStartup(Entity* self) {
+	self->AddTimer("SpawnGoodies", 1);
 }
 
 void XMarksTheSpotChest::OnTimerDone(Entity* self, std::string timerName) {
-    if (timerName == "SpawnGoodies") {
+	if (timerName == "SpawnGoodies") {
+		const auto spawnPosition = self->GetPosition();
+		const auto source = self->GetObjectID();
+		const std::unordered_map<LOT, int32_t> result(
+			{
+				{11916, GeneralUtils::GenerateRandomNumber<uint32_t>(2, 4)}, // life 3 points
+				{11913, GeneralUtils::GenerateRandomNumber<uint32_t>(2, 4)}, // armor 3 points
+				{11910, GeneralUtils::GenerateRandomNumber<uint32_t>(2, 4)} // imagination 3 points
+			}
+		);
 
-        const auto spawnPosition = self->GetPosition();
+		auto* entity = self->GetParentEntity();
 
-        const auto source = self->GetObjectID();
+		for (const auto& [item, count] : result) {
+			GameMessages::DropClientLoot lootMsg{};
+			lootMsg.target = entity->GetObjectID();
+			lootMsg.ownerID = entity->GetObjectID();
+			lootMsg.sourceID = source;
+			lootMsg.item = item;
+			lootMsg.count = 1;
+			lootMsg.spawnPos = spawnPosition;
+			for (int i = 0; i < count; ++i) {
+				Loot::DropItem(*entity, lootMsg, true);
+			}
+		}
 
-        const std::unordered_map<LOT, int32_t> result = std::unordered_map<LOT, int32_t>({{177, GeneralUtils::GenerateRandomNumber<uint32_t>(1, 10)},
-                                                                                          {6431, GeneralUtils::GenerateRandomNumber<uint32_t>(1, 10)},
-                                                                                          {935, GeneralUtils::GenerateRandomNumber<uint32_t>(1, 10)}});
-
-        auto* entity = self->GetParentEntity();
-
-        auto* team = TeamManager::Instance()->GetTeam(entity->GetObjectID());
-
-        std::vector<LWOOBJID> entities = std::vector<LWOOBJID>();
-
-        if (team)
-            entities = team->members;
-        else
-            entities.push_back(entity->GetObjectID());
-
-        for (const auto& objid : entities) {
-            for (const auto& pair : result) {
-                for (int i = 0; i < pair.second; ++i) {
-                    GameMessages::SendDropClientLoot(Game::entityManager->GetEntity(objid), source, pair.first, 0, spawnPosition, 1);
-                }
-            }
-        }
-
-        self->AddTimer("Die", 1);
-    } else if (timerName == "Die") {
-        auto* destComp = self->GetComponent<DestroyableComponent>();
-        if (destComp) {
-            destComp->Smash(self->GetObjectID(), eKillType::SILENT);
-        }
-    }
+		self->AddTimer("Die", 1);
+	} else if (timerName == "Die") {
+		auto* destComp = self->GetComponent<DestroyableComponent>();
+		if (destComp) {
+			destComp->Smash(self->GetObjectID(), eKillType::SILENT);
+		}
+	}
 }
