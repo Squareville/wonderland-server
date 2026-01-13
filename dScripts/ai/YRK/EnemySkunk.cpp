c18d604b58edf9423f7054ab48eaee7777741806
Terrev <21133460+Terrev@users.noreply.github.com>
2026-01-13 01:49:14 -0500

    let nt janitor spray skunks

--- dScripts/ai/YRK/EnemySkunk.cpp
+++ dScripts/ai/YRK/EnemySkunk.cpp
@@ -20,7 +20,7 @@ void EnemySkunk::OnProximityUpdate(Entity* self, Entity* entering, std::string n
 }
 
 void EnemySkunk::OnSkillEventFired(Entity* self, Entity* caster, const std::string& message) {
-	if (message != "waterspray") return;
+	if (message != "waterspray" && message != "soapspray") return;
 
 	auto* const movementAiComponent = self->GetComponent<MovementAIComponent>();
 	if (movementAiComponent) movementAiComponent->Stop();
@@ -28,12 +28,12 @@ void EnemySkunk::OnSkillEventFired(Entity* self, Entity* caster, const std::stri
 	auto* const controllablePhysicsComponent = self->GetComponent<ControllablePhysicsComponent>();
 	if (controllablePhysicsComponent) {
 		controllablePhysicsComponent->ActivateBubbleBuff(eBubbleType::SKUNK);
-		controllablePhysicsComponent->SetVelocity(NiPoint3(0.0f, 5.0f, 0.0f));
+		controllablePhysicsComponent->SetVelocity(NiPoint3(0.0f, 7.0f, 0.0f));
 	}
 
 	Game::entityManager->SerializeEntity(self);
 	const auto casterId = caster->GetObjectID();
-	self->AddCallbackTimer(4.0f, [self, casterId]() {
+	self->AddCallbackTimer(2.0f, [self, casterId]() {
 		if (!self) return;
 		auto* controllablePhysicsComponent = self->GetComponent<ControllablePhysicsComponent>();
 		if (controllablePhysicsComponent) {
